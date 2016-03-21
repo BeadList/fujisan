2120f16c7cada938f630be4a6f94c63c428c2796
Dmitry Yakimov <welldan97@gmail.com>
2016-03-21 23:09:39 +0300

    Initial unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
-gulp.task('test', function() {
+gulp.task('acceptance-test', function() {
   return gulp.src('test/acceptance-test.js', { read: false })
     .pipe(mocha({ reporter: 'spec', timeout: 5000 }));
 });
+
+
+gulp.task('unit-test', function() {
+  return gulp.src('test/unit-tests/**/*-test.js', { read: false })
+    .pipe(mocha({ reporter: 'spec' }));
+});
+
+gulp.task('test', ['acceptance-test', 'unit-test']);
 
 gulp.task('default', ['test']);
---
+++ test/unit-tests/fujisan-test.js
@@ -0,0 +1,46 @@
+/*global require describe it beforeEach */
+
+var process = require('process');
+
+var gulp = require('gulp');
+var expect = require('chai').expect;
+
+var fujisan = require('../../lib/fujisan');
+
+describe('fujisan', function() {
+  describe('.prepareOptions', function() {
+    beforeEach(function () {
+      fujisan.options = undefined;
+    });
+
+    it('sets default options', function () {
+      fujisan.prepareOptions({ gulp: gulp });
+      expect(fujisan.options.config.prefix).to.equal('fuji');
+    });
+
+    it('does not set options if they already set up', function () {
+      fujisan.options = {
+        config: {
+          prefix: 'hola'
+        }
+      };
+      fujisan.prepareOptions({ gulp: gulp });
+      expect(fujisan.options.config.prefix).to.equal('hola');
+    });
+
+    it('sets passed options', function () {
+      fujisan.prepareOptions({ gulp: gulp, config: { prefix: 'zoom' } });
+      expect(fujisan.options.config.prefix).to.equal('zoom');
+    });
+
+    it('sets cwd', function () {
+      fujisan.prepareOptions({ gulp: gulp });
+      expect(fujisan.options.config.cwd).to.equal(process.cwd());
+    });
+
+    it('sets cwd', function () {
+      fujisan.prepareOptions({ gulp: gulp });
+      expect(fujisan.options.config.cwd).to.equal(process.cwd());
+    });
+  });
+});
