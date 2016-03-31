eb3907a406cf5e49755307325b17e79c72006edc
Dmitry Yakimov <welldan97@gmail.com>
2016-03-31 18:56:27 +0300

    Eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const eslint = require('gulp-eslint');
 
 gulp.task('acceptance-test', function() {
   return gulp.src('test/acceptance-test.js', { read: false })
     .pipe(mocha({ reporter: 'spec', timeout: 10000 }));
 });
 
-
 gulp.task('unit-test', function() {
   return gulp.src('test/unit-tests/**/*-test.js', { read: false })
     .pipe(mocha({ reporter: 'spec' }));
 });
 
-gulp.task('test', gulp.parallel('acceptance-test', 'unit-test'));
+gulp.task('lint', () => {
+  return gulp.src(['lib/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', gulp.parallel('lint','acceptance-test', 'unit-test'));
 
 gulp.task('default', gulp.parallel('test'));
